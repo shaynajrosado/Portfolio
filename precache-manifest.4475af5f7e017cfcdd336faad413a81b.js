8d602b40342aec14c158681fcf87fc57fd2186b1
Shayna Rosado <srosado@pace.edu>
2020-04-30 13:34:52 -0400

    Updates

---
+++ precache-manifest.4475af5f7e017cfcdd336faad413a81b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3984a38092b3f3ef9c97c58af6774b65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11c867142e05ff2cfb93",
+    "url": "/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "3a0e1d1365e5b51e8639",
+    "url": "/static/css/main.6efbbdc1.chunk.css"
+  },
+  {
+    "revision": "11c867142e05ff2cfb93",
+    "url": "/static/js/2.e510a80e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.e510a80e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0e1d1365e5b51e8639",
+    "url": "/static/js/main.46804766.chunk.js"
+  },
+  {
+    "revision": "b775e3132e0930477ff9",
+    "url": "/static/js/runtime-main.17a0efde.js"
+  },
+  {
+    "revision": "583ef81530a5bee161ba46dcbbc36006",
+    "url": "/static/media/avatar.583ef815.png"
+  }
+]);
