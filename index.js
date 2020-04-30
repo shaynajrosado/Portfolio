606f401c634b807dd094e61cabd7f0081d4ce881
Shayna Rosado <srosado@pace.edu>
2020-04-30 11:40:36 -0400

    Updates

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'src/index.css';
+import App from 'src/App';
+import * as serviceWorker from 'src/serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { HashRouter } from 'react-router-dom';
---
+++ precache-manifest.0202b33b8252bef3202b84aa39b1803a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3318403b6eca31658fe1533a0d6f28",
+    "url": "/Portfolio/public/index.html"
+  },
+  {
+    "revision": "37788f4b1efa94f14af2",
+    "url": "/Portfolio/public/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "98638d83e96d0c21e9c4",
+    "url": "/Portfolio/public/static/css/main.6efbbdc1.chunk.css"
+  },
+  {
+    "revision": "37788f4b1efa94f14af2",
+    "url": "/Portfolio/public/static/js/2.f7d05e1f.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/public/static/js/2.f7d05e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98638d83e96d0c21e9c4",
+    "url": "/Portfolio/public/static/js/main.114a8c06.chunk.js"
+  },
+  {
+    "revision": "a6a9e61a22c067ec4fdb",
+    "url": "/Portfolio/public/static/js/runtime-main.b11b3d8c.js"
+  },
+  {
+    "revision": "583ef81530a5bee161ba46dcbbc36006",
+    "url": "/Portfolio/public/static/media/avatar.583ef815.png"
+  }
+]);
