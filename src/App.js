2440f9bd50f44afb4eb433971333c175a8a5c8ad
Shayna Rosado <srosado@pace.edu>
2020-04-29 22:59:40 -0400

    Basic skeleton with empty content, but with links and navigation bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './components/main';
+import {Link} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  render() {
+    return (
+      // Uses a header that scrolls with the text, rather than staying locked at the top 
+      <div className="demo-big-content">
+      <Layout>
+          <Header title="Title" scroll>
+            <Navigation>
+              <Link to ="/resume">Resume</Link>
+              <Link to ="/contact">Contact</Link>
+              <Link to ="/aboutme">About Me</Link>
+              <Link to ="/projects">Projects</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Link to ="/resume">Resume</Link>
+              <Link to ="/contact">Contact</Link>
+              <Link to ="/aboutme">About Me</Link>
+              <Link to ="/projects">Projects</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+              <div className="page-content" />
+              <Main></Main>
+          </Content>
+      </Layout>
     </div>
-  );
+    ) 
+  }
 }
-
-export default App;
