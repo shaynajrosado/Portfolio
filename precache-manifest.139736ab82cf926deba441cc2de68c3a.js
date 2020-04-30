1374b8b2be37d7d82a709fc1b0d82fe6bd6a3a0f
Shayna Rosado <srosado@pace.edu>
2020-04-30 14:49:42 -0400

    Updates

---
+++ precache-manifest.139736ab82cf926deba441cc2de68c3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e494003682adf096bd7664d36778aa37",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "37788f4b1efa94f14af2",
+    "url": "/Portfolio/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "6313b8b7ab2ea24afefc",
+    "url": "/Portfolio/static/css/main.6efbbdc1.chunk.css"
+  },
+  {
+    "revision": "37788f4b1efa94f14af2",
+    "url": "/Portfolio/static/js/2.f7d05e1f.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/static/js/2.f7d05e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6313b8b7ab2ea24afefc",
+    "url": "/Portfolio/static/js/main.8bc24289.chunk.js"
+  },
+  {
+    "revision": "4c8b85c1f7e7f3c29fc6",
+    "url": "/Portfolio/static/js/runtime-main.7c70dee3.js"
+  },
+  {
+    "revision": "583ef81530a5bee161ba46dcbbc36006",
+    "url": "/Portfolio/static/media/avatar.583ef815.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4839501fae1562758e10a3623ff23678.js"
+  "/Portfolio/precache-manifest.139736ab82cf926deba441cc2de68c3a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
