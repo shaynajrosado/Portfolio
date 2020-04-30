309abe6aeaf4c3f3fee680c310cd7a5e25e3fe56
Shayna Rosado <srosado@pace.edu>
2020-04-30 16:07:45 -0400

    Still struggling to get this hosted

---
+++ build/precache-manifest.35a7e0b00250a73538210b70b5813412.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c1fbe4612ebdab654c8def81659e41",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "11c867142e05ff2cfb93",
+    "url": "/Portfolio/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "a2805c504411e7ab5aa9",
+    "url": "/Portfolio/static/css/main.6efbbdc1.chunk.css"
+  },
+  {
+    "revision": "11c867142e05ff2cfb93",
+    "url": "/Portfolio/static/js/2.e510a80e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/static/js/2.e510a80e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2805c504411e7ab5aa9",
+    "url": "/Portfolio/static/js/main.3347c65a.chunk.js"
+  },
+  {
+    "revision": "4c8b85c1f7e7f3c29fc6",
+    "url": "/Portfolio/static/js/runtime-main.7c70dee3.js"
+  },
+  {
+    "revision": "583ef81530a5bee161ba46dcbbc36006",
+    "url": "/Portfolio/static/media/avatar.583ef815.png"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
